ffc1de7976d9c9896eeac9cb42695002f08f42a5
Alexander Scheurer <mail@aspepex.net>
2016-08-28 19:48:36 +0200

    start menu almost done

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Default");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowAbout()
+    {
+        //switch to About canvas
+    }
+    public void ShowStart()
+    {
+        //switch to Start canvas
+    }
+
+
+}
